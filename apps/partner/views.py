e5d6bfcf21db578169baa57b88e7c2b0693f3096
Rafal Denisiuk <rrafal889@gmail.com>
2025-05-09 09:01:31 +0200

    Separated partner_form to partner_create_form (and fixed it) and partner_update_form which still needs fixing.

--- apps/partner/views.py
+++ apps/partner/views.py
@@ -1,118 +1,27 @@
-# from django.shortcuts import render, redirect, get_object_or_404
-# from django.urls import reverse, reverse_lazy
-# from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-# from django.contrib import messages
-# from django.utils.translation import gettext_lazy as _
-# from django.contrib.auth.mixins import LoginRequiredMixin
-
-# from .models import Partner, PartnerEmail
-# from .forms import PartnerForm, PartnerEmailForm
-# from django_countries import countries
-
-
-# class PartnerListView(LoginRequiredMixin, ListView):
-#     """Lista partnerów"""
-#     model = Partner
-#     template_name = 'partner/partner_list.html'
-#     context_object_name = 'partners'
-#     paginate_by = 10
-#     ordering = ['name']
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-
-#         # Pobierz wszystkie kraje
-#         all_countries = list(countries)
-
-#         # Przygotuj posortowaną listę krajów
-#         # 1. Polska na początku
-#         # 2. Najczęściej wybierane kraje EU posortowane alfabetycznie
-#         # 3. Pozostałe kraje posortowane alfabetycznie
-
-#         # Usuń Polskę z listy krajów
-#         poland = None
-#         for code, name in all_countries:
-#             if code == 'PL':
-#                 poland = (code, name)
-#                 break
-
-#         if poland:
-#             all_countries.remove(poland)
-
-#         # Najczęściej wybierane kraje UE
-#         frequently_used = [
-#             'DE', 'GB', 'FR', 'ES', 'IT', 'NL', 'BE', 'AT', 'CZ', 'SK', 'LT', 'LV', 'EE'
-#         ]
-
-#         freq_countries = []
-#         remaining_countries = []
-
-#         # Podziel kraje na "często używane" i "pozostałe"
-#         for code, name in all_countries:
-#             if code in frequently_used:
-#                 freq_countries.append((code, name))
-#             else:
-#                 remaining_countries.append((code, name))
-
-#         # Sortuj alfabetycznie
-#         freq_countries.sort(key=lambda x: x[1])  # Sortuj po nazwie kraju
-#         remaining_countries.sort(key=lambda x: x[1])  # Sortuj po nazwie kraju
-
-#         # Złóż listę z powrotem, zaczynając od Polski
-#         sorted_countries = []
-#         if poland:
-#             sorted_countries.append(poland)
-#         sorted_countries.extend(freq_countries)
-#         sorted_countries.extend(remaining_countries)
-
-#         context['countries'] = sorted_countries
-#         return context
-
-
-# class PartnerUpdateView(LoginRequiredMixin, UpdateView):
-#     """Edycja partnera"""
-#     model = Partner
-#     form_class = PartnerForm
-#     template_name = 'partner/partner_update.html'
-
-#     def get_success_url(self):
-#         return reverse('partner_detail', kwargs={'pk': self.object.pk})
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-
-#         # Dodaj listę krajów do kontekstu
-#         context['countries'] = list(countries)
-
-#         return context
-
-
-# class PartnerDeleteView(LoginRequiredMixin, DeleteView):
-#     """Usunięcie partnera"""
-#     model = Partner
-#     template_name = 'partner_confirm_delete.html'
-#     success_url = reverse_lazy('partner_list')
-
-#     def delete(self, request, *args, **kwargs):
-#         messages.success(request, _("Partner został usunięty."))
-#         return super().delete(request, *args, **kwargs)
-
-# apps/partner/views.py
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_POST, require_GET
 from django_countries import countries
 from django.utils import timezone
+from django.db.models import Q
+import logging
+
 
 from .models import Partner, PartnerEmail, VATVerificationHistory
 from .forms import PartnerCreateForm, PartnerFilterForm
+from .vat_service import VATService
+from .utils import add_to_context
+
 from apps.subscriber.models import Subscriber
 from apps.core.vat_verification import VATVerificationService
+
+logger = logging.getLogger(__name__)
 
 
 class PartnerListView(LoginRequiredMixin, ListView):
@@ -120,51 +29,88 @@ class PartnerListView(LoginRequiredMixin, ListView):
     model = Partner
     template_name = 'partner/partner_list.html'
     context_object_name = 'partners'
-    paginate_by = 10
+    paginate_by = 50  # Domyślna liczba rekordów na stronę
     ordering = ['name']
 
     def get_queryset(self):
         queryset = super().get_queryset()
 
-        # Apply filters from form
+        # Pobierz parametry filtrowania
         search_query = self.request.GET.get('search', '')
-        country = self.request.GET.get('country', '')
-        status = self.request.GET.get('status', '')
+        country_filters = self.request.GET.getlist('country')
+        city_filters = self.request.GET.getlist('city')
+        status_filter = self.request.GET.get('status', '')
 
+        # Zastosuj filtrowanie wyszukiwania
         if search_query:
-            queryset = queryset.filter(name__icontains=search_query)
+            queryset = queryset.filter(
+                Q(name__icontains=search_query) |
+                Q(vat_number__icontains=search_query) |
+                Q(city__icontains=search_query)
+            )
 
-        if country:
-            queryset = queryset.filter(country=country)
+        # Zastosuj filtrowanie krajów
+        if country_filters:
+            queryset = queryset.filter(country__in=country_filters)
 
-        if status == 'verified':
+        # Zastosuj filtrowanie miast
+        if city_filters:
+            queryset = queryset.filter(city__in=city_filters)
+
+        # Zastosuj filtrowanie statusu VAT
+        if status_filter == 'verified':
             queryset = queryset.filter(is_verified=True)
-        elif status == 'unverified':
+        elif status_filter == 'unverified':
             queryset = queryset.filter(is_verified=False)
+
+        # Zastosuj paginację na podstawie wybranego rozmiaru strony
+        page_size = self.request.GET.get('page_size')
+        if page_size and page_size.isdigit():
+            self.paginate_by = int(page_size)
 
         return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        # Prepare countries for filter form
-        all_countries = list(countries)
+        # Użyj funkcji add_to_context, żeby dodać spolszczone nazwy krajów
+        context = add_to_context(context)
 
-        # Prepare filter form
+        # Pobierz już posortowane i spolszczone kraje
+        sorted_countries = context['countries']
+
+        # Przygotowanie formularza filtrowania (zachowujemy dla kompatybilności wstecznej)
         form = PartnerFilterForm(
             self.request.GET or None,
-            countries=all_countries
+            countries=sorted_countries
         )
 
+        # Dodanie parametrów wyszukiwania do kontekstu
         context['filter_form'] = form
-        context['countries'] = all_countries
+        context['search_query'] = self.request.GET.get('search', '')
+        context['selected_countries'] = self.request.GET.getlist('country')
+        context['selected_cities'] = self.request.GET.getlist('city')
+        context['selected_status'] = self.request.GET.get('status', '')
+        context['selected_page_size'] = self.request.GET.get(
+            'page_size', str(self.paginate_by))
+        context['default_page_size'] = str(self.paginate_by)
+
+        # Pobierz unikalne miasta dla filtra
+        # Wykorzystaj set comprehension dla unikalnych wartości i posortuj
+        cities = sorted({p.city for p in Partner.objects.filter(
+            city__isnull=False).exclude(city='')})
+        context['cities'] = cities
+
         return context
 
 
 @login_required
 def partner_create_view(request):
     """View for creating new partner"""
-    all_countries = list(countries)
+    # Używamy funkcji tłumaczącej nazwy krajów
+    context = {}
+    context = add_to_context(context)
+    countries_list = context['countries']
 
     if request.method == 'POST':
         form = PartnerCreateForm(request.POST)
@@ -221,19 +167,24 @@ def partner_create_view(request):
     else:
         form = PartnerCreateForm()
 
-    return render(request, 'partner/partner_form.html', {
+    context.update({
         'form': form,
-        'countries': all_countries,
         'subscribers': Subscriber.objects.all(),
         'is_new': True
     })
+
+    return render(request, 'partner/partner_create_form.html', context)
 
 
 @login_required
 def partner_update_view(request, pk):
     """View for updating partner"""
     partner = get_object_or_404(Partner, pk=pk)
-    all_countries = list(countries)
+
+    # Używamy funkcji tłumaczącej nazwy krajów
+    context = {}
+    context = add_to_context(context)
+    countries_list = context['countries']
 
     if request.method == 'POST':
         form = PartnerCreateForm(request.POST, instance=partner)
@@ -271,14 +222,15 @@ def partner_update_view(request, pk):
         partner=partner
     ).order_by('-verification_date')[:10]
 
-    return render(request, 'partner/partner_form.html', {
+    context.update({
         'form': form,
         'partner': partner,
-        'countries': all_countries,
         'subscribers': Subscriber.objects.all(),
         'is_new': False,
         'verification_history': verification_history
     })
+
+    return render(request, 'partner/partner_update_form.html', context)
 
 
 @login_required
@@ -322,3 +274,62 @@ def verify_vat_view(request, pk):
             "Nie udało się zweryfikować numeru VAT."))
 
     return redirect('partner:partner_update', pk=pk)
+
+
+@login_required
+@require_GET
+def fetch_mf_data(request):
+    """
+    Endpoint do pobierania danych z API Ministerstwa Finansów
+    """
+    nip = request.GET.get('nip', '')
+
+    if not nip:
+        return JsonResponse({
+            'success': False,
+            'message': 'Brak numeru NIP'
+        })
+
+    # Wywołanie serwisu do weryfikacji VAT
+    success, data, message, verification_id = VATService.verify_vat(nip)
+
+    return JsonResponse({
+        'success': success,
+        'data': data,
+        'message': message
+    })
+
+
+# views.py
+@login_required
+def fetch_vies_data(request):
+    """Pobieranie danych z API VIES EU"""
+    country = request.GET.get('country', '')
+    vat = request.GET.get('vat', '')
+
+    if not country or not vat:
+        return JsonResponse({
+            'success': False,
+            'message': 'Wymagane są parametry country i vat'
+        })
+
+    # Usuń znaki formatujące z numeru VAT
+    vat = ''.join(c for c in vat if c.isalnum())
+
+    # Wywołaj serwis weryfikacji VAT
+    from apps.core.vat_verification import VATVerificationService
+    success, data, message, verification_id = VATVerificationService._verify_eu_vat(
+        country, vat)
+
+    if success and data:
+        return JsonResponse({
+            'success': True,
+            'data': data,
+            'message': 'Numer VAT został zweryfikowany pomyślnie w systemie VIES EU.',
+            'verification_id': verification_id
+        })
+    else:
+        return JsonResponse({
+            'success': False,
+            'message': message or 'Nie udało się zweryfikować numeru VAT w systemie VIES EU.'
+        })
